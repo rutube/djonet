3cdcebe4be70ab57fa2956351bd2c36fa106f73d
Mark Bucciarelli <mark@x30.crosscutmedia.com>
2009-12-16 18:41:30 -0500

    comment tweaks

--- django_monetdb/creation.py
+++ django_monetdb/creation.py
@@ -2,6 +2,7 @@
 from django.db.backends.creation import BaseDatabaseCreation
 
 class DatabaseCreation(BaseDatabaseCreation):
+
 	#
 	# XXX: I didn't see where to define the max int values that fit
 	# XXX: in the int fields.  The Positive* variants will hold less
@@ -24,8 +25,14 @@ class DatabaseCreation(BaseDatabaseCreation):
 	    'IPAddressField'		: 'char(15)',
 	    'NullBooleanField'		: 'boolean',
 	    'OneToOneField'		: 'int',
+	    #
+	    # XXX: MINVALUE seemed promising for the positive integer
+	    # XXX: fields, but the first try didn't work and I didn't
+	    # XXX: pursue it further.
+	    #
 	    #'PositiveIntegerField'	: 'int MINVALUE 0',
 	    #'PositiveSmallIntegerField'	: 'smallint MINVALUE 0',
+	    #
 	    'PositiveIntegerField'	: 'int',
 	    'PositiveSmallIntegerField'	: 'smallint',
 	    'SlugField'			: 'varchar(%(max_length)s)',
