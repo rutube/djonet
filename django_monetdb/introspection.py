37c9d0bf5622a299d1876dc430436569a556cbe0
Mark Bucciarelli <mark@x30.crosscutmedia.com>
2009-12-16 18:39:11 -0500

    whitespace

--- django_monetdb/introspection.py
+++ django_monetdb/introspection.py
@@ -1,22 +1,20 @@
 from django.db.backends import BaseDatabaseIntrospection
-import re
-
-foreign_key_re = re.compile(r"\sCONSTRAINT `[^`]*` FOREIGN KEY \(`([^`]*)`\) REFERENCES `([^`]*)` \(`([^`]*)`\)")
 
 class DatabaseIntrospection(BaseDatabaseIntrospection):
-    data_types_reverse = {
-        'boolean'	: 'BooleanField',
-        'varchar'	: 'CharField',
-        'date'		: 'DateField',
-        'timestamp'	: 'DateTimeField',
-        'numeric'	: 'FloatField',
-        'int'		: 'IntegerField',
-        'smallint'	: 'IntegerField',
-        'clob'		: 'TextField',
-        'time'		: 'TimeField',
-    }
 
-    def get_table_list(self, cursor):
-        "Returns a list of table names in the current database."
-        cursor.execute("select name from sys.tables;")
-        return [row[0] for row in cursor.fetchall()]
+	data_types_reverse = {
+	    'boolean'	: 'BooleanField',
+	    'varchar'	: 'CharField',
+	    'date'	: 'DateField',
+	    'timestamp'	: 'DateTimeField',
+	    'numeric'	: 'FloatField',
+	    'int'	: 'IntegerField',
+	    'smallint'	: 'IntegerField',
+	    'clob'	: 'TextField',
+	    'time'	: 'TimeField',
+	}
+
+	def get_table_list(self, cursor):
+		'''Return a list of table names in the current database.'''
+		cursor.execute("select name from sys.tables;")
+		return [row[0] for row in cursor.fetchall()]
