8bdb8e5a2ddc28adbf35f2790e579cae8c35a3ce
Gijs Molenaar <gijs@pythonic.nl>
2012-07-31 14:44:43 +0200

    updated version

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
+#!/usr/bin/env python
 
 from distutils.core import setup
+
 setup(name='djonet',
-    version='0.1',
+    version='0.2',
     description='MonetDB backend for Django',
     author='Gijs Molenaar',
     author_email='gijsmolenaar@gmail.com',
     url='https://github.com/gijzelaerr/djonet',
     packages=['djonet'],
-    requires=['monetdb', 'django']
+    requires=['python-monetdb', 'django']
 )
