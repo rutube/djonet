20b2e614f113e41defc7534f70f1edf5b96de9a2
Gijs Molenaar <gijs@pythonic.nl>
2013-06-07 16:18:48 +0200

    flush command is using

--- djonet/operations.py
+++ djonet/operations.py
@@ -16,27 +16,71 @@
 #
 
 import re
-
-from django.db.backends import util
 from django.db.backends import BaseDatabaseOperations
+from djonet.introspection import DatabaseIntrospection
+
+
+def toposort(data):
+    """Dependencies are expressed as a dictionary whose keys are items
+and whose values are a set of dependent items. Output is a list of
+sets in topological order. The first set consists of items with no
+dependences, each subsequent set consists of items that depend upon
+items in the preceeding sets.
+
+found on:
+http://code.activestate.com/recipes/578272-topological-sort/
+
+>>> print '\\n'.join(repr(sorted(x)) for x in toposort2({
+...     2: set([11]),
+...     9: set([11,8]),
+...     10: set([11,3]),
+...     11: set([7,5]),
+...     8: set([7,3]),
+...     }) )
+[3, 5, 7]
+[8, 11]
+[2, 9, 10]
+"""
+
+    from functools import reduce
+
+    # Ignore self dependencies.
+    for k, v in data.items():
+        v.discard(k)
+    # Find all items that don't depend on anything.
+    extra_items_in_deps = reduce(set.union, data.itervalues()) - set(data.iterkeys())
+    # Add empty dependences where needed
+    data.update({item:set() for item in extra_items_in_deps})
+    while True:
+        ordered = set(item for item, dep in data.iteritems() if not dep)
+        if not ordered:
+            break
+        yield ordered
+        data = {item: (dep - ordered)
+                for item, dep in data.iteritems()
+                    if item not in ordered}
+
+    assert not data, "Cyclic dependencies exist among these items:\n%s" %\
+                     '\n'.join(repr(x) for x in data.iteritems())
+
 
 class DatabaseOperations(BaseDatabaseOperations):
     compiler_module = "djonet.compiler"
 
     operators = {
-        'gt'        : '>  %s',
-        'gte'        : '>= %s',
-        'lt'        : '<  %s',
-        'lte'        : '<= %s',
-        'exact'        : '=  %s',
-        'startswith'    : 'LIKE %s',
-        'istartswith'    : 'ILIKE %s',
-        'iexact'        : 'ILIKE %s',
-        'icontains'        : 'ILIKE %s',
+        'gt': '>  %s',
+        'gte': '>= %s',
+        'lt': '<  %s',
+        'lte': '<= %s',
+        'exact': '=  %s',
+        'startswith': 'LIKE %s',
+        'istartswith': 'ILIKE %s',
+        'iexact': 'ILIKE %s',
+        'icontains': 'ILIKE %s',
     }
 
     def quote_name(self, name):
-        '''Return quoted name of table, index or column name.'''
+        """Return quoted name of table, index or column name."""
         name = re.sub('-', '', name)
         if name.startswith('"') and name.endswith('"'):
             return name
@@ -50,29 +94,52 @@ class DatabaseOperations(BaseDatabaseOperations):
 
         The `style` argument is a Style object as returned by either
         color_style() or no_style() in django.core.management.color.
+
+        Since we can't disable foreign key checks, we need to do some sort
+        of ordering. Topological sorting is applied to make sure no referred
+        foreign keys are deleted. This only works in a-cycling references.
         """
+        if not tables:
+            return []
+
+        introspection = DatabaseIntrospection(self.connection)
+        cursor = self.connection.connection.cursor()
+
+        all_relations = {}
+        for table in tables:
+            relations = introspection.get_relations(cursor, table)
+            for field, other_table in relations.values():
+                if other_table not in all_relations:
+                    all_relations[other_table] = [table]
+                else:
+                    all_relations[other_table].append(table)
+
+        all_relations = dict([(k, set(v)) for k, v in all_relations.items()])
+        sorted_topo = toposort(all_relations)
+        sorted_tables = [j for i in sorted_topo for j in i]
+
         sql = []
-        if tables:
-            for table in tables:
-                sql.append('%s %s;' % (style.SQL_KEYWORD('DELETE FROM'), style.SQL_FIELD(self.quote_name(table))))
+        for table in sorted_tables:
+            sql.append('%s %s;' % (style.SQL_KEYWORD('DELETE FROM'),
+                                   style.SQL_FIELD(self.quote_name(table)))
+                       )
         return sql
 
-
     def date_extract_sql(self, lookup_type, field_name):
-        '''Given a lookup_type of 'year', 'month' or 'day',
+        """Given a lookup_type of 'year', 'month' or 'day',
         returns the SQL that extracts a value from the given
         date field field_name.
-            '''
+            """
         return "EXTRACT(%s FROM %s)" % (lookup_type, field_name)
 
     def start_transaction_sql(self):
-        '''MonetDB uses START TRANSACTION not BEGIN.'''
+        """MonetDB uses START TRANSACTION not BEGIN."""
         return 'START TRANSACTION;'
 
     def model_to_sequencesql(self, m):
-        '''Make the SQL statement that updates the tables primary
+        """Make the SQL statement that updates the tables primary
         key sequence.  Return empty string if no SQL needed.
-        '''
+        """
         from django.db import connection
 
         # tbl has app_label prefix; e.g., testapp_simple
@@ -81,7 +148,7 @@ class DatabaseOperations(BaseDatabaseOperations):
         # Get name of sequence for this table.  Here's
         # a trace from doing it manually.
         #
-        #     sql>  select "default" from sys.columns 
+        #     sql>  select "default" from sys.columns
         #     more> where table_id = 4186 and name = 'id';
         #     +-------------------------------------+
         #     | default                             |
@@ -93,17 +160,17 @@ class DatabaseOperations(BaseDatabaseOperations):
         #
 
         c = connection.cursor()
-        fmt = '''
-SELECT 
-  "default" 
-FROM 
-  sys.columns 
-WHERE 
+        fmt = """
+SELECT
+  "default"
+FROM
+  sys.columns
+WHERE
   table_id = (SELECT id FROM sys.tables where name = %s) AND
   name = 'id'
 ;
-'''
-        c.execute(fmt, [tbl,])
+"""
+        c.execute(fmt, [tbl, ])
         row = c.fetchone()
         # default = 'next value for "django1"."seq_4176"'
         default = row[0]
@@ -119,97 +186,10 @@ WHERE
         return fmt % (seq, tbl)
 
     def sequence_reset_sql(self, style, model_list):
-        '''Return list of sequence update SQL for given models.
-
-        Style is how to color the output for a terminal??!!'''
-
-        from django.db import models
-
-        fmt = "ALTER SEQUENCE %s RESTART WITH (SELECT MAX(id) + 1 FROM %s);"
-
-        rval = []
+        """Return list of sequence update SQL for given models."""
+        results = []
         for model in model_list:
             sql = self.model_to_sequencesql(model)
             if sql:
-                rval.append(sql)
-        return rval
-
-
-#    def date_trunc_sql(self, lookup_type, field_name):
-#        fields = ['year', 'month', 'day', 'hour', 'minute', 'second']
-#        format = ('%%Y-', '%%m', '-%%d', ' %%H:', '%%i', ':%%s') 
-#        format_def = ('0000-', '01', '-01', ' 00:', '00', ':00')
-#        try:
-#            i = fields.index(lookup_type) + 1
-#        except ValueError:
-#            sql = field_name
-#        else:
-#            format_str = ''.join([f for f in format[:i]] + [f for f in format_def[i:]])
-#            sql = "CAST(DATE_FORMAT(%s, '%s') AS DATETIME)" % (field_name, format_str)
-#        return sql
-#
-#    def drop_foreignkey_sql(self):
-#        return "DROP FOREIGN KEY"
-#
-#    def fulltext_search_sql(self, field_name):
-#        return 'MATCH (%s) AGAINST (%%s IN BOOLEAN MODE)' % field_name
-#
-#    def no_limit_value(self):
-#        # 2**64 - 1, as recommended by the MySQL documentation
-#        return 18446744073709551615L
-#
-
-#
-#    def random_function_sql(self):
-#        return 'RAND()'
-#
-#    def sql_flush(self, style, tables, sequences):
-#        # NB: The generated SQL below is specific to MySQL
-#        # 'TRUNCATE x;', 'TRUNCATE y;', 'TRUNCATE z;'... style SQL statements
-#        # to clear all tables of all data
-#        if tables:
-#            sql = ['SET FOREIGN_KEY_CHECKS = 0;']
-#            for table in tables:
-#                sql.append('%s %s;' % (style.SQL_KEYWORD('TRUNCATE'), style.SQL_FIELD(self.quote_name(table))))
-#            sql.append('SET FOREIGN_KEY_CHECKS = 1;')
-#
-#            # 'ALTER TABLE table AUTO_INCREMENT = 1;'... style SQL statements
-#            # to reset sequence indices
-#            sql.extend(["%s %s %s %s %s;" % \
-#                (style.SQL_KEYWORD('ALTER'),
-#                 style.SQL_KEYWORD('TABLE'),
-#                 style.SQL_TABLE(self.quote_name(sequence['table'])),
-#                 style.SQL_KEYWORD('AUTO_INCREMENT'),
-#                 style.SQL_FIELD('= 1'),
-#                ) for sequence in sequences])
-#            return sql
-#        else:
-#            return []
-#
-#    def value_to_db_datetime(self, value):
-#        if value is None:
-#            return None
-#        
-#        # MySQL doesn't support tz-aware datetimes
-#        if value.tzinfo is not None:
-#            raise ValueError("MySQL backend does not support timezone-aware datetimes.")
-#
-#        # MySQL doesn't support microseconds
-#        return unicode(value.replace(microsecond=0))
-#
-#    def value_to_db_time(self, value):
-#        if value is None:
-#            return None
-#            
-#        # MySQL doesn't support tz-aware datetimes
-#        if value.tzinfo is not None:
-#            raise ValueError("MySQL backend does not support timezone-aware datetimes.")
-#        
-#        # MySQL doesn't support microseconds
-#        return unicode(value.replace(microsecond=0))
-#
-#    def year_lookup_bounds(self, value):
-#        # Again, no microseconds
-#        first = '%s-01-01 00:00:00'
-#        second = '%s-12-31 23:59:59.99'
-#        return [first % value, second % value]
+                results.append(sql)
+        return results
